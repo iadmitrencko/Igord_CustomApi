b7cbdb8130f4f24f7443212800dfac38069f2980
iadmitrencko <iadmitrencko@gmail.com>
2018-10-09 20:14:12 +0300

    helper for work with config

--- app/code/community/Igord/CustomApi/Helper/Data.php
+++ app/code/community/Igord/CustomApi/Helper/Data.php
@@ -2,5 +2,17 @@
 
 class Igord_CustomApi_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    protected $supportEmailPath = "siteblocks/support/email";
 
+    protected $supportOldRequestDays = "siteblocks/support/clear_old/days";
+
+    public function getSupportEmail()
+    {
+        return Mage::getConfig()->getNode($this->supportEmailPath);
+    }
+
+    public function getSupportOldRequestDays()
+    {
+        return Mage::getConfig()->getNode($this->supportOldRequestDays);
+    }
 }
